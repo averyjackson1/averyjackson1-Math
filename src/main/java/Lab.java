906be4596d32b0ba95ca950ba14de91738a405f6
averyjackson1 <jackson.avery11@gmail.com>
2023-02-03 22:27:01 +0000

    Done

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -19,7 +19,12 @@ public class Lab {
      *
      * @return return a^b using the pow() method of the Math class.
      */
-    public double mathPow(double a, double b){
-        return 0;
+    public static double mathPow(double a, double b){
+      
+       
+        return Math.pow(a,b);
     }
-}
+
+    
+    }
+
